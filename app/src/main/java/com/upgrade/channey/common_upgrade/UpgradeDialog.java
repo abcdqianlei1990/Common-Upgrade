43c017a6c3612fefc472896bb05061573af14ad0
钱雷 <qianlei01@shengdaxd.com>
2017-03-02 16:52:25 +0800

    1.增加强制更新设置 2.ui优化

--- app/src/main/java/com/upgrade/channey/common_upgrade/UpgradeDialog.java
+++ app/src/main/java/com/upgrade/channey/common_upgrade/UpgradeDialog.java
@@ -1,11 +1,13 @@
 package com.upgrade.channey.common_upgrade;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Message;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.text.TextUtils;
 import android.util.Log;
@@ -24,25 +26,27 @@ import com.daimajia.numberprogressbar.OnProgressBarListener;
 public class UpgradeDialog {
     private static final String TAG = "UpgradeDialog";
     private static UpgradeDialog INSTANCE = null;
-    private Context context;
+    private Activity activity;
     private AlertDialog dialog;
     private TextView content;
     private NumberProgressBar bar;
     private TextView negBtn;
     private TextView posBtn;
+    private TextView noticeTv;
     private static final int MAX_PROGRESS = 100;
     private int mDownLoadStatus = -1;
     private static final int DOWNLOAD_STATUS_START = 0;
     private static final int DOWNLOAD_STATUS_DOWNLOADING = 1;
     private static final int DOWNLOAD_STATUS_FAILURE = 2;
     private static final int DOWNLOAD_STATUS_END = 3;
+    private boolean focusUpdate = false;
     private Handler mHandler = new Handler(){
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             switch (msg.what){
                 case DOWNLOAD_STATUS_FAILURE:
-                    Toast.makeText(context,"下载失败^_^!!",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(activity,"下载失败^_^!!",Toast.LENGTH_SHORT).show();
 //                    posBtn.setText("重新下载");
                     break;
                 case DOWNLOAD_STATUS_DOWNLOADING:
@@ -50,14 +54,14 @@ public class UpgradeDialog {
                     bar.setProgress(progress);
                     break;
                 case DOWNLOAD_STATUS_END:
-                    Toast.makeText(context,"下载完成",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(activity,"下载完成",Toast.LENGTH_SHORT).show();
                     dialog.dismiss();
                     break;
             }
         }
     };
 
-   public static UpgradeDialog getInstance(@NonNull Context context){
+   public static UpgradeDialog getInstance(@NonNull Activity context){
        if(INSTANCE == null){
            synchronized (UpgradeDialog.class){
                if(INSTANCE == null){
@@ -68,8 +72,8 @@ public class UpgradeDialog {
        return INSTANCE;
    }
 
-    private UpgradeDialog(@NonNull Context ctx){
-        context = ctx;
+    private UpgradeDialog(@NonNull Activity ctx){
+        activity = ctx;
     }
 
     /**
@@ -78,19 +82,23 @@ public class UpgradeDialog {
      * @param msg message to show
      * @param apkLink 安装包链接
      * @param authority android sdk 7.0需要的file provider authority
+     * notice:该方法请最后调用
      */
-    public void show(String msg,final String apkLink,final String authority){
+    public void show(String msg,final String apkLink,@Nullable final String authority){
+        if(activity.isFinishing()){
+            return;
+        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             if (TextUtils.isEmpty(authority)){
                 Log.e(TAG,"need file provider authority.");
                 return;
             }
         }
-        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         dialog = builder.create();
         dialog.show();
         dialog.setCanceledOnTouchOutside(false);
-        View view = LayoutInflater.from(context).inflate(R.layout.pop_upgrade_dialog,null);
+        View view = LayoutInflater.from(activity).inflate(R.layout.pop_upgrade_dialog,null);
         dialog.getWindow().setContentView(view);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable());
 
@@ -98,8 +106,14 @@ public class UpgradeDialog {
         bar = (NumberProgressBar) view.findViewById(R.id.upgrade_dialog_progressBar);
         negBtn = (TextView) view.findViewById(R.id.upgrade_dialog_neg_btn);
         posBtn = (TextView) view.findViewById(R.id.upgrade_dialog_pos_btn);
+        noticeTv = (TextView) view.findViewById(R.id.upgrade_dialog_notice);
 
         content.setText(msg);
+        if(focusUpdate){
+            noticeTv.setVisibility(View.VISIBLE);
+        }else {
+            noticeTv.setVisibility(View.GONE);
+        }
         negBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -111,10 +125,11 @@ public class UpgradeDialog {
             @Override
             public void onClick(View v) {
                 bar.setVisibility(View.VISIBLE);
+                noticeTv.setVisibility(View.GONE);
                 if(mDownLoadStatus == DOWNLOAD_STATUS_START || mDownLoadStatus == DOWNLOAD_STATUS_DOWNLOADING){
-                    Toast.makeText(context,"下载已开始",Toast.LENGTH_LONG).show();
+                    Toast.makeText(activity,"下载已开始",Toast.LENGTH_LONG).show();
                 }else {
-                    UpdateUtil.downLoadApk(context, new UpdateUtil.DownloadListener() {
+                    UpdateUtil.downLoadApk(activity, new UpdateUtil.DownloadListener() {
                         @Override
                         public void startDownload() {
                             mDownLoadStatus = DOWNLOAD_STATUS_START;
@@ -125,7 +140,7 @@ public class UpgradeDialog {
                         public void downloadSuccess() {
                             mDownLoadStatus = DOWNLOAD_STATUS_END;
                             Log.d("qian","downloadSuccess");
-                            UpdateUtil.installApk(context,authority);
+                            UpdateUtil.installApk(activity,authority);
                         }
 
                         @Override
@@ -152,6 +167,15 @@ public class UpgradeDialog {
                 }
             }
         });
+        dialog.setOnDismissListener(new DialogInterface.OnDismissListener() {
+            @Override
+            public void onDismiss(DialogInterface dialog) {
+                if(focusUpdate){
+                    activity.finish();
+                    System.exit(0);
+                }
+            }
+        });
         initProgressBar();
     }
 
@@ -162,9 +186,20 @@ public class UpgradeDialog {
             public void onProgressChange(int current, int max) {
                 if(current == max){
                     dialog.dismiss();
-                    Toast.makeText(context,"下载完成,即将安装",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(activity,"下载完成,即将安装",Toast.LENGTH_SHORT).show();
                 }
             }
         });
+    }
+
+    /**
+     * 是否强制更新，强制更新时，用户不更新将无法进入app
+     * 请谨慎使用
+     * notice:该方法调用请放在{@link #show(String, String, String)}前面
+     * @param focus 是否强制更新
+     */
+    public UpgradeDialog focusUpdate(boolean focus){
+        focusUpdate = focus;
+        return this;
     }
 }
--- testDemo/src/main/java/com/upgrade/channey/test/MainActivity.java
+++ testDemo/src/main/java/com/upgrade/channey/test/MainActivity.java
@@ -12,12 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //7.0以下版本
-//        UpgradeDialog.getInstance(this).show("有新版本","http://118.178.248.230:8888/app-dxd-debug.apk",null);
 
-        //7.0
-        String authority = "com.upgrade.channey.test.fileProvider";
-        Toast.makeText(this,authority,Toast.LENGTH_LONG).show();
-        UpgradeDialog.getInstance(this).show("有新版本","http://118.178.248.230:8888/app-dxd-debug.apk",authority);
+        //7.0版本 authority不能为空，7.0以下版本authority可为空
+        String authority = "com.upgrade.channey.test.fileProvider"; //和manifest中provider的authority一致
+        UpgradeDialog.getInstance(this).focusUpdate(false).show("有新版本","http://118.178.248.230:8888/app-dxd-debug.apk",authority);
     }
 }
